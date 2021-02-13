f9397e5af48ef0c0e294865eaae2ae686ed319ec
SpencerLeung1024 <spencerleung1024@gmail.com>
2021-02-13 13:28:30 -0800

    rocket

--- app/src/main/java/ca/cmpt276/cosmos/game.java
+++ app/src/main/java/ca/cmpt276/cosmos/game.java
@@ -38,6 +38,9 @@ public class game extends AppCompatActivity {
         int spaceshipY = maxY - 300;
         Log.i("spaceship x", ""+ spaceshipX);
         Log.i("spaceship Y", ""+ spaceshipY);
+        double spaceshipForwardVel = 0;
+        double spaceshipVelX = 0;
+        double spaceshipVelY = 0;
         Spaceship spaceship = new Spaceship(spaceshipX,spaceshipY);
         setSpaceshipLocation(spaceship.getX(), spaceship.getY());
     }
@@ -47,6 +50,30 @@ public class game extends AppCompatActivity {
         spaceshipIcon.setX(x);
         spaceshipIcon.setY(y);
     }
+
+    private void setSpaceshipRotation(int r) {
+        ImageView spaceshipIcon = findViewById(R.id.spaceship);
+        spaceshipIcon.setRotation(r);
+    }
+
+    // TODO: consider whether it would be better to use polar coordinates (angle from planet, radius) instead of cartesian coordinates
+    // as you can see, this is absolutely broken
+    private void step(double stepTime) {
+        ImageView spaceshipIcon = findViewById(R.id.spaceship);
+        double x = spaceshipIcon.getX();
+        double y = spaceshipIcon.getY();
+        double r = spaceshipIcon.getRotation();
+        spaceshipVelX = spaceshipForwardVel * Math.sin(Math.toRadians(r));
+        spaceshipVelY = spaceshipForwardVel * Math.cos(Math.toRadians(r));
+        double newX = x + (spaceshipVelX * stepTime);
+        double newY = y + (spaceshipVelY * stepTime);
+        setSpaceshipLocation((int) newX, (int) newY);
+        double newR = Math.atan2(newY, newX) + 1.570796327;
+        setSpaceshipRotation((int) newR);
+        // TODO: adjust rotation based on where the planet actually is. the rocket currently only orbits (0, 0)
+    }
+
+
 
 
 }
