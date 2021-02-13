aa2d49639ef3d481bae62ae55a5cc5d255b378db
Yu Zheng <jimsonz@sfu.ca>
2021-02-13 12:58:27 -0800

    spawn spaceship on bottom of screen

--- app/src/main/java/ca/cmpt276/cosmos/game.java
+++ app/src/main/java/ca/cmpt276/cosmos/game.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -23,7 +25,20 @@ public class game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Spaceship spaceship = new Spaceship(0,0);
+
+        DisplayMetrics display = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(display);
+
+        int maxX = (int) display.widthPixels;
+        int maxY = (int) display.heightPixels;
+        ImageView spaceshipIcon = findViewById(R.id.spaceship);
+        Log.i("x", ""+maxX);
+        Log.i("Y", ""+ maxY);
+        int spaceshipX = maxX/2 - 50;
+        int spaceshipY = maxY - 300;
+        Log.i("spaceship x", ""+ spaceshipX);
+        Log.i("spaceship Y", ""+ spaceshipY);
+        Spaceship spaceship = new Spaceship(spaceshipX,spaceshipY);
         setSpaceshipLocation(spaceship.getX(), spaceship.getY());
     }
 
